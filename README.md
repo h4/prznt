fc159dbf0a2c04073ac386decefd2f92486627b7
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-07-16 21:16:19 +0400

    Base functionality

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ prznt
 =====
 
 Simple presentation template
+
+Use arrow keys to navigate
+Press `L` key or `Space` to show list of slides
---
+++ js/prznt.js
@@ -0,0 +1,82 @@
+var PRZNT = {};
+
+PRZNT.init = function () {
+    this.slides = $('.slide');
+    this.currentSlide = 0;
+    this.page_number = $('.page-number');
+    this.toc = {
+        box: $('.toc-box'),
+        list: $('.toc'),
+        closer: $('.toc-box .closer')
+    };
+
+    $('.pages-count').text(this.slides.length);
+
+    this.toc.closer.click(this.toggleTOC);
+
+    for (var i=0, l=this.slides.length; i<l; i++) {
+        $("<li></li>")
+        .text(PRZNT.slides.eq(i)
+        .attr('data-title'))
+        .click(function (e) {
+            PRZNT.currentSlide = $(e.target).index();
+            PRZNT.go();
+            PRZNT.toggleTOC();
+            return false;
+        })
+        .appendTo(this.toc.list);
+    }
+
+
+    $(document).keyup(function(e) {
+        switch (e.keyCode) {
+            case 39:
+            case 40:
+                PRZNT.next();
+                break;
+
+            case 37:
+            case 38:
+                PRZNT.prev();
+                break;
+            case 32:
+            case 76:
+                PRZNT.toggleTOC();
+                break;
+        }
+    });
+
+    this.go();
+};
+
+PRZNT.next = function() {
+    if(this.currentSlide === (this.slides.length - 1)) {
+        return;
+    }
+
+    this.currentSlide ++;
+    this.go();
+};
+
+PRZNT.prev = function() {
+    if(this.currentSlide === 0) {
+        return;
+    }
+
+    this.currentSlide --;
+    this.go();
+};
+
+PRZNT.go = function() {
+    location.hash = '#' + this.slides.eq(this.currentSlide).attr('id');
+
+    this.page_number.text(this.currentSlide + 1);
+};
+
+PRZNT.toggleTOC = function() {
+    PRZNT.toc.box.toggleClass('toc-box-hidden');
+};
+
+$(function() {
+    PRZNT.init();
+});
